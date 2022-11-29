db48913444897a5118827671355ffd45b2c8231d
theQuert <ga11004@cs.nccu.edu.tw>
2022-11-29 20:20:22 +0800

    Add some replacement conditions

--- util/netku_edit_labeled_fix.py
+++ util/netku_edit_labeled_fix.py
@@ -7,6 +7,19 @@ val_labeled_pt = torch.load('../dataset/val_log/val_labeled.pt')
 wrap_pt = []
 for idx in range(len(train_labeled_pt)):
     idx_content = {}
-    idx_content['content'] = train_labeled_pt[idx]['content'].replace('[KEEP] ### [KEEP] ## [KEEP]', '#####').replace('# ####', '#####').replace('##### ', '#####')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace('# ####', '#####')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace('##### ', '#####')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace(' [KEEP] ### [KEEP] ##', '.##### [KEEP]')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace('.##### [KEEP] [KEEP] ', '.##### [KEEP] ')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace('[KEEP] #####', '##### [KEEP] ')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace('##### [KEEP] [KEEP] ', '##### [KEEP] ')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace(' ##### [KEEP]', '.##### [KEEP]')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace('..#####', '.#####')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace(' [ADD] #####', '##### [ADD] ')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace(' [SUB] #####', '##### [SUB] ')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace(' [KEEP] #### [KEEP] #', '##### [KEEP] ')
+    train_labeled_pt[idx]['content'] = train_labeled_pt[idx]['content'].replace('[KEEP] [ADD]', '[KEEP]')
+
+    idx_content['content'] = train_labeled_pt[idx]['content']
     wrap_pt.append(idx_content)
 torch.save(wrap_pt, './train_labeled_fixed.pt')
