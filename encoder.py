045e6d094386017bc615e05bb98dcc663ae4819c
theQuert <ga11004@cs.nccu.edu.tw>
2023-02-19 22:20:04 +0800

    Update encoder script

---
+++ encoder.py
@@ -0,0 +1,331 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sn
+import warnings
+import nltk
+import random, time
+import datetime
+# nltk.download("stopwords")
+from nltk.corpus import stopwords
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler,random_split
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import transformers
+from transformers import BartForSequenceClassification, AdamW, BartTokenizer, get_linear_schedule_with_warmup
+
+if torch.cuda.is_available():       
+    device = torch.device("cuda")
+    print(f'There are {torch.cuda.device_count()} GPU(s) available.')
+    print('Device name:', torch.cuda.get_device_name(0))
+
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
+
+df = pd.read_csv('/home/quert/edit_NetKu/dataset/same_secs_insert_labeled/resample_train.csv')
+df.info()
+
+df = df[["paragraph", "target"]]
+df.info()
+
+# Import test and val data
+# /home/quert/edit_NetKu/dataset/same_secs_insert_labeled
+test_data = pd.read_csv('/home/quert/edit_NetKu/dataset/same_secs_insert_labeled/merged_updated_test.csv')
+val_data = pd.read_csv('/home/quert/edit_NetKu/dataset/same_secs_insert_labeled/merged_updated_val.csv')
+test_data = test_data[['paragraph', 'target']]
+val_data = test_data[['paragraph', 'target']]
+data_train = df
+data_val = val_data
+# 
+X_train = data_train.paragraph.values
+y_train = data_train.target.values
+labels = data_train.target.values
+X_val = data_val.paragraph.values
+y_val = data_val.target.values
+
+# Data preprocessing
+
+def text_preprocessing(s):
+    """
+    - Lowercase the sentence
+    - Change "'t" to "not"
+    - Remove "@name"
+    - Isolate and remove punctuations except "?"
+    - Remove other special characters
+    - Remove stop words except "not" and "can"
+    - Remove trailing whitespace
+    """
+    s = s.lower()
+    # Change 't to 'not'
+    s = re.sub(r"\'t", " not", s)
+    # Remove @name
+    s = re.sub(r'(@.*?)[\s]', ' ', s)
+    # Isolate and remove punctuations except '?'
+    s = re.sub(r'([\'\"\.\(\)\!\?\\\/\,])', r' \1 ', s)
+    s = re.sub(r'[^\w\s\?]', ' ', s)
+    # Remove some special characters
+    s = re.sub(r'([\;\:\|•«\n])', ' ', s)
+    # Remove stopwords except 'not' and 'can'
+    s = " ".join([word for word in s.split()
+                  if word not in stopwords.words('english')
+                  or word in ['not', 'can']])
+    # Remove trailing whitespace
+    s = re.sub(r'\s+', ' ', s).strip()
+    
+    return s
+
+def text_preprocessing(text):
+    """
+    - Remove entity mentions (eg. '@united')
+    - Correct errors (eg. '&amp;' to '&')
+    @param    text (str): a string to be processed.
+    @return   text (Str): the processed string.
+    """
+    # Remove '@name'
+    text = re.sub(r'(@.*?)[\s]', ' ', text)
+
+    # Replace '&amp;' with '&'
+    text = re.sub(r'&amp;', '&', text)
+
+    # Remove trailing whitespace
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    return text
+
+tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn', do_lower_case=True)
+max_len = 0
+for sent in X_train:
+    input_ids = tokenizer.encode(sent, add_special_tokens=True)
+    max_len = max(max_len, len(input_ids))
+print('Max sentence length: ', max_len)
+
+def preprocessing_bart(data, labels):
+    input_ids = []
+    attention_masks = []
+    for sent in data:
+        # `encode_plus` will:
+        #    (1) Tokenize the sentence
+        #    (2) Add the `[CLS]` and `[SEP]` token to the start and end
+        #    (3) Truncate/Pad sentence to max length
+        #    (4) Map tokens to their IDs
+        #    (5) Create attention mask
+        #    (6) Return a dictionary of outputs
+        encoded_dict = tokenizer.encode_plus(
+            text=text_preprocessing(sent),  # Preprocess sentence
+            add_special_tokens=True,        # Add `[CLS]` and `[SEP]`
+            max_length=600,                  # Max length to truncate/pad
+            pad_to_max_length=True,         # Pad sentence to max length
+            return_tensors='pt',           # Return PyTorch tensor
+            return_attention_mask=True,
+            truncation=True
+            )
+
+        # Add the outputs to the lists
+        input_ids.append(encoded_dict.get('input_ids'))
+        attention_masks.append(encoded_dict.get('attention_mask'))
+    # Convert lists to tensors
+    # input_ids = torch.LongTensor(input_ids)
+    # attention_masks = torch.LongTensor(attention_masks)
+    # labels = torch.tensor(labels)
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+    # labels = torch.tensor(labels)
+    # labels = torch.tensor(labels).unsqueeze(0)
+    labels = torch.LongTensor(labels)
+    return input_ids, attention_masks, labels
+
+# Convert the data into TensorDataset
+input_ids, attention_masks, labels = preprocessing_bart(X_train, y_train)
+train_dataset = TensorDataset(input_ids, attention_masks, labels)
+input_ids, attention_masks, labels = preprocessing_bart(X_val, y_val)
+val_dataset = TensorDataset(input_ids, attention_masks, labels)
+
+# Create DataLoader
+batch_size = 16
+
+train_dataloader = DataLoader(
+            train_dataset,
+            sampler = RandomSampler(train_dataset),
+            batch_size = batch_size
+        )
+
+validation_dataloader = DataLoader(
+            val_dataset,
+            sampler = SequentialSampler(val_dataset),
+            batch_size = batch_size
+        )
+
+model = BartForSequenceClassification.from_pretrained(
+    "facebook/bart-large-cnn",
+    num_labels = 2,
+    output_attentions = False,
+    output_hidden_states = False,
+)
+
+model = model.to(device)
+
+optimizer = AdamW(model.parameters(),
+                  lr = 2e-5,
+                  eps = 1e-8
+                )
+
+epochs = 3
+
+# Total number of training steps is [number of batches] x [number of epochs]. 
+# (Note that this is not the same as the number of training samples).
+total_steps = len(train_dataloader) * epochs
+
+# Create the learning rate scheduler.
+scheduler = get_linear_schedule_with_warmup(optimizer, 
+                                            num_warmup_steps = 0, # Default value in run_glue.py
+                                            num_training_steps = total_steps)
+
+# Function to calculate the accuracy of our predictions vs labels
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+def format_time(elapsed):
+    '''
+    Takes a time in seconds and returns a string hh:mm:ss
+    '''
+    # Round to the nearest second.
+    elapsed_rounded = int(round((elapsed)))
+    # Format as hh:mm:ss
+    return str(datetime.timedelta(seconds=elapsed_rounded))
+
+seed_val = 42
+random.seed(seed_val)
+np.random.seed(seed_val)
+torch.manual_seed(seed_val)
+torch.cuda.manual_seed_all(seed_val)
+training_stats = []
+
+# Measure the total training time for the whole run.
+total_t0 = time.time()
+
+# For each epoch...
+for epoch_i in range(0, epochs):
+
+    # ========================================
+    #               Training
+    # ========================================
+    # Perform one full pass over the training set.
+    print("")
+    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
+    print('Training...')
+    # Measure how long the training epoch takes.
+    t0 = time.time()
+    total_train_loss = 0
+    model.train()
+    for step, batch in enumerate(train_dataloader):
+        # Unpack this training batch from our dataloader.
+        #
+        # As we unpack the batch, we'll also copy each tensor to the device using the
+        # `to` method.
+        #
+        # `batch` contains three pytorch tensors:
+        #   [0]: input ids
+        #   [1]: attention masks
+        #   [2]: labels
+        b_input_ids = batch[0].to(device)
+        b_input_mask = batch[1].to(device)
+        b_labels = batch[2].to(device)
+        optimizer.zero_grad()
+        output = model(b_input_ids,
+                       attention_mask=b_input_mask,
+                       labels=b_labels)
+        loss = output.loss
+        total_train_loss += loss.item()
+        # Perform a backward pass to calculate the gradients.
+        loss.backward()
+        # Clip the norm of the gradients to 1.0.
+        # This is to help prevent the "exploding gradients" problem.
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        # Update parameters and take a step using the computed gradient.
+        # The optimizer dictates the "update rule"--how the parameters are
+        # modified based on their gradients, the learning rate, etc.
+        optimizer.step()
+        # Update the learning rate.
+        scheduler.step()
+
+    # Calculate the average loss over all of the batches.
+    avg_train_loss = total_train_loss / len(train_dataloader)
+
+    # Measure how long this epoch took.
+    training_time = format_time(time.time() - t0)
+    print("")
+    print("  Average training loss: {0:.2f}".format(avg_train_loss))
+    print("  Training epcoh took: {:}".format(training_time))
+    # ========================================
+    #               Validation
+    # ========================================
+    # After the completion of each training epoch, measure our performance on
+    # our validation set.
+    print("")
+    print("Running Validation...")
+    t0 = time.time()
+    # Put the model in evaluation mode--the dropout layers behave differently
+    # during evaluation.
+    model.eval()
+    # Tracking variables
+    total_eval_accuracy = 0
+    best_eval_accuracy = 0
+    total_eval_loss = 0
+    nb_eval_steps = 0
+    # Evaluate data for one epoch
+    for batch in validation_dataloader:
+        b_input_ids = batch[0].to(device)
+        b_input_mask = batch[1].to(device)
+        b_labels = batch[2].to(device)
+        # Tell pytorch not to bother with constructing the compute graph during
+        # the forward pass, since this is only needed for backprop (training).
+        with torch.no_grad():
+            output= model(b_input_ids,
+                          attention_mask=b_input_mask,
+                          labels=b_labels)
+        loss = output.loss
+        total_eval_loss += loss.item()
+        # Move logits and labels to CPU if we are using GPU
+        logits = output.logits
+        logits = logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+        # Calculate the accuracy for this batch of test sentences, and
+        # accumulate it over all batches.
+        total_eval_accuracy += flat_accuracy(logits, label_ids)
+    # Report the final accuracy for this validation run.
+    avg_val_accuracy = total_eval_accuracy / len(validation_dataloader)
+    print("  Accuracy: {0:.2f}".format(avg_val_accuracy))
+    # Calculate the average loss over all of the batches.
+    avg_val_loss = total_eval_loss / len(validation_dataloader)
+    # Measure how long the validation run took.
+    validation_time = format_time(time.time() - t0)
+    if avg_val_accuracy > best_eval_accuracy:
+        torch.save(model, 'bart_model')
+        best_eval_accuracy = avg_val_accuracy
+    #print("  Validation Loss: {0:.2f}".format(avg_val_loss))
+    #print("  Validation took: {:}".format(validation_time))
+    # Record all statistics from this epoch.
+    training_stats.append(
+        {
+            'epoch': epoch_i + 1,
+            'Training Loss': avg_train_loss,
+            'Valid. Loss': avg_val_loss,
+            'Valid. Accur.': avg_val_accuracy,
+            'Training Time': training_time,
+            'Validation Time': validation_time
+        }
+    )
+print("")
+print("Training complete!")
+
+print("Total training took {:} (h:mm:ss)".format(format_time(time.time()-total_t0)))
