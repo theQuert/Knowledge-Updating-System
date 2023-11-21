efb9bad504eb893e646499408b1e2a7ca79b74d5
theQuert <ga11004@cs.nccu.edu.tw>
2023-11-21 12:17:03 +0800

    Update gcp config file > update in KGE repo

--- gcp_config.sh
+++ gcp_config.sh
@@ -2,23 +2,28 @@ sudo apt update
 sudo apt autoremove -y
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.8 -y
-# install cuda version 11.7
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2004-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt install python3.10 -y
+
+# install cuda 12.3
+# referece: https://developer.nvidia.com/cuda-downloads?target_os=Linux
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
-# sudo apt-get -y install cuda
-# sudo apt -y install nvidia-cuda-toolkit
+sudo apt-get -y install cuda-toolkit-12-3
+
+sudo apt-get update
+sudo apt-get -y install cuda
 sudo add-apt-repository -y ppa:graphics-drivers/ppa && sudo apt update
+sudo apt -y install nvidia-cuda-toolkit
+sudo apt install ubuntu-drivers-common
 sudo ubuntu-drivers autoinstall
 sudo reboot
-# check nvidia-smi
+# check nvidia-smi and nvcc
+nvidia-smi
+nvcc -V
+
 # prepare torch environment
 sudo apt install -y python3-pip
 sudo pip3 install torch torchvision torchaudio
 # install venv
-sudo apt install python3.8-venv
-
+sudo apt install python3.10-venv
